50be981ba7e41cb356032d2792d5abb91e01a93b
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2024-08-11 21:19:07 -0500

    Changes - Updates

---
+++ astro.config.mjs
@@ -0,0 +1,4 @@
+import { defineConfig } from "astro/config";
+
+//Todo https://astro.build/config
+export default defineConfig({});
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+
+//Todo https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [],
+});
